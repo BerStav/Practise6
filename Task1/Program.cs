3048b2819fca0e5ca4742ff653483821b4506a35
Vitaly <Vitalion.Bersenev@gmail.com>
2022-12-18 19:31:17 +0700

    task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Please, enter the several numbers separated by space: ");
+
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int amount = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        amount++;
+    }
+}
+
+Console.WriteLine($"amount of numbers greater than zero : {amount}");
